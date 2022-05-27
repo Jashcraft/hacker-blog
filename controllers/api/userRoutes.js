41db2ff54ba1e9257420a3bcff36e463e939d26e
Jonathon Ashcraft <ashcraft.jonathon@gmail.com>
2022-05-26 23:45:03 -0600

    Authorization

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,15 +1,40 @@
 const router = require('express').Router();
-const {User} = require ('../../models')
+const { User } = require('../../models')
 
 router.post('/', async (req, res) => {
-    try {
-        const {username, email, password} = req.body;
-        const data = await User.create({username, email, password});
-    
-        res.json(data);
-    } catch(err) {
-        res.json(err)
-    }
+  try {
+    const { username, email, password } = req.body;
+    const userData = await User.create({ username, email, password });
+
+    res.json(userData);
+  } catch (err) {
+    res.json(err)
+  }
+})
+
+router.post('/login', async (req, res) => {
+  const {username, password} = req.body;
+  console.log('req', req)
+  try {
+      const userData = await User.findOne({
+        where: {
+          username: username
+        }
+      })
+      if(!userData){
+        return res.status(400).json('Invalid Credentials');
+      }
+
+      const isValidPw = await userData.checkPassword(password);
+      if(!isValidPw){
+        return res.status(400).json('Invalid Credentials');
+      }
+
+      return res.json({user: userData, message: 'Logged In'})
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
 })
 
 module.exports = router;
--- public/js/auth.js
+++ public/js/auth.js
@@ -1,17 +1,13 @@
-const signupButton = document.querySelector('#signup');
-const emailEl = document.querySelector('#email');
-const usernameEl = document.querySelector('#username');
-const passwordEl = document.querySelector('#password');
-
-const signupHandler = async (e) => {
+const signupHandler = async e => {
     e.preventDefault();
-    const username = usernameEl.value.trim();
-    const password = passwordEl.value.trim();
-    const email = passwordEl.value.trim();
+    const username = document.querySelector('#username').value.trim();
+    const password = document.querySelector('#password').value.trim();
+    const email = document.querySelector('#email').value.trim();
     if(username && password && email){
         const signupConfirmed = await fetch('/api/users', {
             method: 'POST',
-            body: JSON.stringify({username, password, email})
+            body: JSON.stringify({username, password, email}),
+            headers: { 'Content-Type': 'application/json' }
         })
 
         if(signupConfirmed.ok){
@@ -23,4 +19,30 @@ const signupHandler = async (e) => {
 
 }
 
-signupButton.addEventListener('click', signupHandler)
+const loginHandler = async e => {
+    e.preventDefault();
+    const username = document.querySelector('#username').value.trim();
+    const password = document.querySelector('#password').value.trim();
+
+    if(username && password){
+        const login = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: { 'Content-Type': 'application/json' }
+        })
+
+        if(login.ok){
+            document.location.replace('/');
+        } else {
+            alert('Invalid Credentials');
+        }
+    }
+}
+
+console.log(window.location.pathname);
+
+if(window.location.pathname === '/login'){
+    document.querySelector('#login').addEventListener('click', loginHandler);
+} else if (window.location.pathname === '/signup') {
+    document.querySelector('#signup').addEventListener('click', signupHandler);
+}
